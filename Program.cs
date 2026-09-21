0736814e99da0ecf9b1ce17e8ac75257ceee91cd
alimamed0807 <alimamed0807@gmail.com>
2026-09-21 18:54:58 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(() =>
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("---------LAST 20 PROCESSES---------");
+
+                var processList = new List<Process>();
+
+                foreach (var p in Process.GetProcesses())
+                {
+                    try
+                    {
+                        var time = p.StartTime;
+                        processList.Add(p);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                var processes = processList
+                    .OrderByDescending(p => p.StartTime)
+                    .Take(20);
+
+                foreach (var p in processes)
+                {
+                    Console.WriteLine($"{p.ProcessName} - {p.Id} - {p.StartTime}");
+                }
+
+                Thread.Sleep(500);
+            }
+        });
+
+        thread1.Start();
+
+        Thread thread2 = new Thread(() =>
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter command: ");
+
+                string command = Console.ReadLine();
+
+                string[] parts = command.Split(' ');
+
+                if (parts[0] == "Start")
+                {
+                    Process.Start(parts[1]);
+                }
+                else if (parts[0] == "Kill")
+                {
+                    foreach (var p in Process.GetProcessesByName(
+                        Path.GetFileNameWithoutExtension(parts[1])))
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+        });
+
+        thread2.Start();
+    }
+}
